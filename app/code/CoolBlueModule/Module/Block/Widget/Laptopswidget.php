b3fd58fb749ba3eae3578eda2b8d4e6fc66d3148
Valia <ValiaStohnii@github.com>
2022-09-29 16:48:45 +0300

    laptops widget

---
+++ app/code/CoolBlueModule/Module/Block/Widget/Laptopswidget.php
@@ -0,0 +1,9 @@
+<?php
+namespace CoolBlueModule\Module\Block\Widget;
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+
+class Laptopswidget extends Template implements BlockInterface
+{
+    protected $_template = "widget/laptopswidget.phtml";
+}
